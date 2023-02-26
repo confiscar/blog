4ccfa72ec5f83a7bfb0ffddd257073ed7da56fc5
confiscar <24923564+confiscar@users.noreply.github.com>
2023-02-26 16:05:36 +0000

    add default placeholder

--- src/components/molecules/ImageCard/index.tsx
+++ src/components/molecules/ImageCard/index.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
 import Card from '../../atoms/Card';
 import style from './ImageCard.module.css';
+import defaultPlaceholder from './defaultPlaceholder';
 
 declare type ImageCardProps = {
-    item: { title: string, link: string, cardImage?: string }
+    item: { title: string, link: string, cardImage?: string, base64CardImage?: string}
 }
 
 const cardHeight = 180;
@@ -13,6 +14,7 @@ const imageHeight = cardHeight / 1.6;
 const NoImageHeader = () => (
     <div style={{width: cardWidth, height: imageHeight}} className={style.cardHeaderNoImage} />
 );
+const generateDataUrl = (data: string) => `data:image/jpeg;base64,${data}`;
 
 const ImageCard = ({ item }: ImageCardProps) => {
     return (
@@ -24,6 +26,8 @@ const ImageCard = ({ item }: ImageCardProps) => {
                         height={imageHeight}
                         src={item.cardImage}
                         alt=""
+                        placeholder="blur"
+                        blurDataURL={generateDataUrl(defaultPlaceholder)}
                     />
                     : <NoImageHeader/>
                 }
